b66b29d5510da17d5db87218cdf66052aa1ff237
Yanako Lajara <yanakolajara@pursuit.org>
2023-07-07 18:56:36 -0400

    Not found route added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { Route, Routes } from 'react-router-dom';
 import { Homepage } from './Homepage/Homepage';
 import { Show } from './Show/Show';
 import { Create } from './Create/Create';
+import { NotFound } from './NotFound/NotFound';
+import { ErrorPage } from './ErrorPage/ErrorPage'
 import { useNavigate } from 'react-router-dom'
 
 function App() {
@@ -18,6 +20,8 @@ function App() {
         <Route path='/' element={<Homepage/>}/>
         <Route path='/show/:id' element={<Show/>}/>
         <Route path='/create' element={<Create/>}/>
+        <Route path='/notfound' element={<NotFound/>}/>
+        <Route path='*' element={<ErrorPage/>}/>
       </Routes>
     </div>
   );
